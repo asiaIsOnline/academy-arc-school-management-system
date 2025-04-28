8fe0fe6f0efb5dc8defd86d9c5bf245695ff36cb
asiasAtWork <asiasatwork@gmail.com>
2025-04-27 18:00:48 -0700

    Add components and base styling for side navigation

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,7 @@
+import Image from "next/image";
+import Link from "next/link";
+import Menu from "../components/Menu";
+
 export default function DashboardLayout({
     children,
   }: Readonly<{
@@ -6,9 +10,24 @@ export default function DashboardLayout({
     return (
         <div className="h-screen flex">
             {/* LEFT */}
-            <div className="w-1/6 bg-green-200">l</div>
+            <div className="md-w-1/5 bg-slate-50">l
+              <div>
+                <Link href="/" className="flex flex-col items-center gap-2">
+                  <Image 
+                    src="/academy-arc-pictorial-logo.svg"
+                    alt="academy arc pictorial logo light version"
+                    className="px-2"
+                    width={154}
+                    height={56}
+                  />
+                  <h2 className="text-center text-2xl hidden lg:block">Academy Arc</h2>
+                  <h3 className="text-center text-sm hidden xl:block">School Management System</h3>
+                </Link>
+                <Menu />
+              </div>
+            </div>
             {/* RIGHT */}
-            <div className="w-5/6 bg-slate-400">r</div>
+            <div className="w-5/6 bg-slate-100">r</div>
         </div>
     );
   }
---
+++ src/app/components/Menu.tsx
@@ -0,0 +1,116 @@
+import React from 'react'
+import Link from 'next/link';
+import Image from 'next/image';
+
+const menuItems = [
+  {
+    title: "MENU",
+    items: [
+      {
+        icon: "/home.png",
+        label: "Home",
+        href: "/",
+      },
+      {
+        icon: "/teacher.png",
+        label: "Teachers",
+        href: "/teachers",
+      },
+      {
+        icon: "/student.png",
+        label: "Students",
+        href: "/students",
+      },
+      {
+        icon: "/parent.png",
+        label: "Parents",
+        href: "/parents",
+      },
+      {
+        icon: "/class.png",
+        label: "Classes",
+        href: "/classes",
+      },
+      {
+        icon: "/lesson.png",
+        label: "Lessons",
+        href: "/lessons",
+      },
+      {
+        icon: "/exam.png",
+        label: "Exams",
+        href: "/exams",
+      },
+      {
+        icon: "/assignment.png",
+        label: "Assignments",
+        href: "/assignments",
+      },
+      {
+        icon: "/attendance.png",
+        label: "Attendance",
+        href: "/attendance",
+      },
+      {
+        icon: "/calendar.png",
+        label: "Events",
+        href: "/events",
+      },
+      {
+        icon: "/message.png",
+        label: "Messages",
+        href: "/messages",
+      },
+      {
+        icon: "/announcement.png",
+        label: "Accouncements",
+        href: "/accouncements",
+      },
+    ]
+  },
+  {
+    title: "OTHER",
+    items: [
+      {
+        icon: "/profile.png",
+        label: "Profile",
+        href: "/profile",
+      },
+      {
+        icon: "/setting.png",
+        label: "Settings",
+        href: "/settings",
+      },
+      {
+        icon: "/logout.png",
+        label: "Logout",
+        href: "/logout",
+      },
+    ]
+  }
+];
+
+const Menu = () => {
+  return (
+    <div className='mt-4 px-6 text-base'>
+      {menuItems.map(i => (
+        <div className='mt-4' key={i.title}>
+          <h4 className='mb-2 hidden lg:block'>{i.title}</h4>
+          {i.items.map(item => (
+            <Link href={item.href} key={item.label} className='flex gap-2 p-2 justify-center lg:justify-start'>
+              <Image
+                src={item.icon}
+                alt=''
+                width={20}
+                height={20}
+              />
+              <span className='hidden lg:block'>{item.label}</span>
+            </Link>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default Menu
